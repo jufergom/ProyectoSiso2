ec90474463aec5ece74ca6fd1bcf8af20cd621d4
Julio <jufergom@hotmail.com>
2020-02-04 14:53:19 -0600

    added listing files on a directory and added .gitignore

--- main.cpp
+++ main.cpp
@@ -2,12 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//for listing files on directory
+#include <dirent.h> 
+//for string and vector
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct FileShow {
+    string name;
+    int x;
+    int y;
+    bool file; //true if it represents a file, false if it represents a folder
+};
+
+vector<string> getFilesOnDirectory(string directory) {
+    DIR *d;
+    struct dirent *dir;
+    vector<string> files;
+    d = opendir(directory.c_str());
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            if(dir->d_name[0] != '.') {
+                //printf("%s\n", dir->d_name);
+                std::string str(dir->d_name);
+                files.push_back(str);
+            }
+                
+        }
+        closedir(d);
+    } 
+    return files;
+}
 
 int main() {
     Display *display;
     Window window;
     XEvent event;
-    const char *msg = "Hello, World!";
+    char *msg = (char *)"Hello, World!";
     int s;
  
     /* open connection with the server */
@@ -20,7 +53,7 @@ int main() {
     s = DefaultScreen(display);
  
     /* create window */
-    window = XCreateSimpleWindow(display, RootWindow(display, s), 10, 10, 200, 200, 1,
+    window = XCreateSimpleWindow(display, RootWindow(display, s), 10, 10, 800, 600, 1,
                            BlackPixel(display, s), WhitePixel(display, s));
  
     /* select kind of events we are interested in */
@@ -35,8 +68,15 @@ int main() {
  
         /* draw or redraw the window */
         if (event.type == Expose) {
-            XFillRectangle(display, window, DefaultGC(display, s), 20, 20, 10, 10);
-            XDrawString(display, window, DefaultGC(display, s), 50, 50, msg, strlen(msg));
+            vector<string> files = getFilesOnDirectory("/home/julio");
+            for(int i = 0; i < files.size(); i++) {
+                XFillRectangle(display, window, DefaultGC(display, s), 2, 20*(i+1)-10, 15, 10);
+                XDrawString(display, window, DefaultGC(display, s), 20, 20*(i+1), 
+                        files[i].c_str(), strlen(files[i].c_str()));
+            }
+            //XFillRectangle(display, window, DefaultGC(display, s), 20, 20, 10, 10);
+            //XDrawString(display, window, DefaultGC(display, s), 50, 50, msg, strlen(msg));
+
         }
         /* exit on key press */
         if (event.type == KeyPress)
@@ -47,4 +87,4 @@ int main() {
     XCloseDisplay(display);
  
     return 0;
- }
+}
